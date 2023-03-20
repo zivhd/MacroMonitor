d2af09eead2893fe8f5bde55a4e5da7b862f4f40
Xei-pher <seiferboado101@gmail.com>
2023-03-20 20:04:33 +0800

    Finalized data models

---
+++ MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/data/model/Calories.kt
@@ -0,0 +1,10 @@
+package ph.edu.dlsu.mobdeve.boado.rodriguez.mco3.data.model
+
+import java.time.LocalDate
+import java.util.*
+
+class Calories(var id: Int) {
+    var userID: Int = 0
+    var totalCalories: Int = 0
+    var date: LocalDate = LocalDate.now()
+}
--- MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/data/model/alarm.kt
+++ MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/data/model/alarm.kt
@@ -3,6 +3,7 @@ package ph.edu.dlsu.mobdeve.boado.rodriguez.mco3.data.model
 import java.sql.Time
 
 class alarm(var id: Int, var time: Time) {
+    var userID: Int = 0
     var meal: String = ""
     var calories: Int = 0
 }
