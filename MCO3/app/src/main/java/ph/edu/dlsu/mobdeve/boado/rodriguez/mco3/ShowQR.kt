f290dd8e2f4fb04b79211160ab4946782ab320d0
zivHD <zivprodriguez@gmail.com>
2023-03-30 15:01:45 +0800

    added show QR Code activity

--- MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/ShowQR.kt
+++ MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/ShowQR.kt
@@ -1,11 +1,44 @@
 package ph.edu.dlsu.mobdeve.boado.rodriguez.mco3
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
+import ph.edu.dlsu.mobdeve.boado.rodriguez.mco3.databinding.ActivityQrcodeBinding
+import ph.edu.dlsu.mobdeve.boado.rodriguez.mco3.databinding.ActivityShowQrBinding
 
 class ShowQR : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityShowQrBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_qr)
+        binding = ActivityShowQrBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val ssid = intent.getStringExtra("ssid")!!
+        binding.idIVQrcode.setImageBitmap(getQrCodeBitmap(ssid))
+        binding.goBackBtn.setOnClickListener(){
+            val intent = Intent(this, AlarmActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun getQrCodeBitmap(ssid: String): Bitmap {
+        val size = 512 //pixels
+        val hints = hashMapOf<EncodeHintType, Int>().also { it[EncodeHintType.MARGIN] = 1 } // Make the QR code buffer border narrower
+        val bits = QRCodeWriter().encode(ssid, BarcodeFormat.QR_CODE, size, size)
+        return Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
+            for (x in 0 until size) {
+                for (y in 0 until size) {
+                    it.setPixel(x, y, if (bits[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+        }
     }
 }
