40c608a09505674f964011e11354a21e7bcc9e7f
Xei-pher <seiferboado101@gmail.com>
2023-04-12 18:53:39 +0800

    started with graphs

--- MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/HomeActivity.kt
+++ MCO3/app/src/main/java/ph/edu/dlsu/mobdeve/boado/rodriguez/mco3/HomeActivity.kt
@@ -41,15 +41,8 @@ class HomeActivity : AppCompatActivity() {
             arrayOf(
                 // on below line we are adding
                 // each point on our x and y axis.
-                DataPoint(0.0, 1.0),
-                DataPoint(1.0, 3.0),
-                DataPoint(2.0, 4.0),
-                DataPoint(3.0, 9.0),
-                DataPoint(4.0, 6.0),
-                DataPoint(5.0, 3.0),
-                DataPoint(6.0, 6.0),
-                DataPoint(7.0, 1.0),
-                DataPoint(8.0, 2.0)
+                DataPoint(date, 1.0),
+                DataPoint(date, 3.0)
             )
         )
         binding.calorieGraph.viewport.isScalable = true
